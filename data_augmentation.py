2d5b7bba1925441ff7d726b41139911e12b5a3be
Gabriele Soldati <soldati.gabriele@gmail.com>
2017-05-08 20:43:07 +0100

    Add files via upload

--- data_augmentation.py
+++ data_augmentation.py
@@ -52,29 +52,29 @@ seq = iaa.Sequential([
 )
 '''
 
-rotate1 = iaa.Affine(rotate=(-90, -90)) # rotate by -45 to +45 degrees)
-rotate2 = iaa.Affine(rotate=(180, 180)) # rotate by -45 to +45 degrees)
-rotate3 = iaa.Affine(rotate=(90, 90)) # rotate by -45 to +45 degrees)
-#bright = iaa.Add((-10, 10), per_channel=0.5) # change brightness of images (by -10 to 10 of original value)
-scale1 = iaa.Affine(scale={"x": (0.8, 0.8), "y": (0.8, 0.8)})
-scale2 = iaa.Affine(scale={"x": (1.4, 1.4), "y": (1.4, 1.4)})
+k = 0
+images = []
+images_aug = []
+rescale = []
+rotation = []
+shearing = []
+
+
 flip1 = iaa.Fliplr(1) # horizontally flip all images
 flip2 = iaa.Flipud(1) # vertically flip all images
-#suppix = iaa.Superpixels(p_replace=(0.9, 1.0), n_segments=(20, 200)) # superpixel representation
 multiply = iaa.Multiply((0.5, 1.5), per_channel=1) # change brightness of images (50-150% of original value)
 blur = iaa.GaussianBlur((1.0, 3.0)) # blur images with a sigma between 0 and 3.0
 contrast = iaa.ContrastNormalization((0.2, 0.9), per_channel=0.5) # improve or worsen the contrast
-shear1 = iaa.Affine(shear=(-16, -16)) # shear by -16 to +16 degrees)
-shear2 = iaa.Affine(shear=(16, 16)) # shear by -16 to +16 degrees)
-shear3 = iaa.Affine(shear=(-26, -26)) # shear by -16 to +16 degrees)
-shear4 = iaa.Affine(shear=(26, 26)) # shear by -16 to +16 degrees)
-shear5 = iaa.Affine(shear=(-36, -36)) # shear by -16 to +16 degrees)
-shear6 = iaa.Affine(shear=(36, 36)) # shear by -16 to +16 degrees)
 
+for i in range(0,360,10):
+    rotation.append(iaa.Affine(rotate=(i, i)))
 
-i = 0
-images = []
-images_aug = []
+for i in range(80,140,10):
+    rescale.append(iaa.Affine(scale={"x": (i/100, i/100), "y": (i/100, i/100)}))
+
+for i in range(0,72,5):
+    shearing.append(iaa.Affine(shear=(i-36, i-36)))
+
 
 directory = "originali/test/"
 save_directory = "modified/"
@@ -88,61 +88,47 @@ for file in os.listdir(directory):
     
 #for batch_idx in range(10):
 #perform the transformations
-
-#rotation
-images_aug.append(rotate3.augment_images(images))
-images_aug.append(rotate2.augment_images(images))
-images_aug.append(rotate1.augment_images(images))
 #brightness
 #images_aug.append(bright.augment_images(images))
-#scale
-images_aug.append(scale1.augment_images(images))
-images_aug.append(scale2.augment_images(images))
+
+for rotate in rotation:
+    images_aug.append(rotate.augment_images(images))
+    images_aug.append(flip1.augment_images(rotate.augment_images(images)))
+    images_aug.append(flip2.augment_images(rotate.augment_images(images)))
+    images_aug.append(multiply.augment_images(rotate.augment_images(images)))
+    images_aug.append(blur.augment_images(rotate.augment_images(images)))
+    images_aug.append(contrast.augment_images(rotate.augment_images(images)))
+
+for scale in rescale:
+    images_aug.append(scale.augment_images(images))
+    images_aug.append(flip1.augment_images(scale.augment_images(images)))
+    images_aug.append(flip2.augment_images(scale.augment_images(images)))
+    images_aug.append(multiply.augment_images(scale.augment_images(images)))
+    images_aug.append(blur.augment_images(scale.augment_images(images)))
+    images_aug.append(contrast.augment_images(scale.augment_images(images)))
+
+for shear in shearing:
+    images_aug.append(shear.augment_images(images))
+    images_aug.append(flip1.augment_images(shear.augment_images(images)))
+    images_aug.append(flip2.augment_images(shear.augment_images(images)))
+    images_aug.append(multiply.augment_images(shear.augment_images(images)))
+    images_aug.append(blur.augment_images(shear.augment_images(images)))
+    images_aug.append(contrast.augment_images(shear.augment_images(images)))
+
 #flip
 images_aug.append(flip1.augment_images(images))
 images_aug.append(flip2.augment_images(images))
-# superpixel
-#images_aug.append(suppix.augment_images(images))
 #moltiplica
 images_aug.append(multiply.augment_images(images))
 #blur
 images_aug.append(blur.augment_images(images))
 #contrast
 images_aug.append(contrast.augment_images(images))
-#shear
-images_aug.append(shear1.augment_images(images))
-images_aug.append(shear2.augment_images(images))
-images_aug.append(shear3.augment_images(images))
-images_aug.append(shear4.augment_images(images))
-images_aug.append(shear5.augment_images(images))
-images_aug.append(shear6.augment_images(images))
-
 #save the images
 for _ in images_aug:
     for img in _:
         rescaled = (255.0 / img.max() * (img - img.min())).astype(np.uint8)
-        imsave(save_directory+str(i)+'.png',rescaled)        
-        i = i+1
+        imsave(save_directory+str(k)+'.png',rescaled)        
+        k = k+1
 
 
-'''
-#images = np.random.randint(0, 255, (16, 128, 128, 3), dtype=np.uint8)
-seq = iaa.Sequential([iaa.Fliplr(0.5), iaa.GaussianBlur((0, 3.0))])
-aug = iaa.WithChannels(
-  channels=[0],
-  children=iaa.Add((-30, 30))
-)
-images_aug = aug.augment_images(images)
-
-print(seq)
-
-# show an image with 8*8 augmented versions of image 0
-#seq.show_grid(images[0], cols=8, rows=8)
-plt.grid()
-plt.show(images[0])
-
-# Show an image with 8*8 augmented versions of image 0 and 8*8 augmented
-# versions of image 1. The identical augmentations will be applied to
-# image 0 and 1.
-#seq.show_grid([images[0], images[1]], cols=8, rows=8)
-'''
